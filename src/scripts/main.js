c924136610448d962c123661afb8763f56ad6246
Akbar Try Hartomo <akbar@codigo.id>
2018-07-02 19:33:08 +0700

    Initiate scripts with ES6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,34 @@
-$(function() {
-  'use-strict';
+import foo from './foo';
 
-  let bootstrapFunction = {
-    init: function() {
-      this.part();
-      this.external();
-      // this.external();
-    },
-
-    external: function() {
-      // $.getScript('./component.js');
-      // component.helloComponent();
-    },
-
-    part: function() {
-      console.log('\'Allo \'Allo!');
-    }
+const bootstrapFunction = (() => {
+  let animateBall = () => {
+    foo();
   };
 
-  $(document).ready(function() {
-    // Render all objects literal
-    bootstrapFunction.init();
-  });
-});
+  // Each function want you to create, make it them to be variable functions
+  // ES6 supports
+
+  let timeOutButton = () => {
+    setTimeout(() => {
+      $('#btnSplendid').css({
+        background: '#F55555',
+        borderRadius: '10em',
+        border: '1px solid #333'
+      });
+    }, 1000);
+    console.log('Hello Time Out Button');
+  };
+
+  let init = () => {
+    timeOutButton();
+    animateBall();
+    // Grab the functions you want activate
+  };
+
+  // Here is to render the function from init variable
+  return {
+    init: init
+  };
+})();
+
+bootstrapFunction.init();
